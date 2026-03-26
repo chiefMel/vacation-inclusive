2c52807dab45049b8c9d346f6a52a2cee71d71bc
Melody Yang <mytkus@gmail.com>
2026-03-26 10:28:20 -0500

    update

--- app/italy/rome-vs-venice/page.tsx
+++ app/italy/rome-vs-venice/page.tsx
@@ -35,7 +35,7 @@ export default function RomeVsVenice() {
                 </div>
             </section>
             <div className="max-w-5xl mx-auto px-6 mt-8">
-                <div className="bg-blue-100 text-white p-4 rounded-xl text-center">
+                <div className="w-full border text-black p-4 rounded-xl text-center">
                     <p className="font-medium mb-2">
                         Compare hotel prices in Rome & Venice for your travel dates
     </p>
