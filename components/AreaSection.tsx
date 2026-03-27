73f71c2bac2c4f07d1d297c2aeb263ffccc4b538
Melody Yang <mytkus@gmail.com>
2026-03-27 09:35:20 -0500

    update

--- components/AreaSection.tsx
+++ components/AreaSection.tsx
@@ -34,7 +34,7 @@ export default function AreaSection({
                         desc={hotel.desc}
                         link={hotel.link}
                         highlight={hotel.highlight}
-                        primaryCta="false"
+                        primaryCta=false
                     />
                 ))}
             </div>
